7c085bb9567d80b5d235f2d6841d84a45c635981
CaoWGG <cwgnlfd@163.com>
2019-07-15 14:58:42 +0800

    add a yolo loss

--- darkyolo_train.py
+++ darkyolo_train.py
@@ -1,7 +1,7 @@
 import os
 os.environ.setdefault('CUDA_VISIBLE_DEVICES','0')
 
-from dataset.yolo_coco import yoloCOCO
+from dataset.yolo_coco_2 import yoloCOCO
 from models.yolo_head import Yolodet
 import darkyolo_config as cfg
 from torch.optim import Adam
@@ -14,4 +14,4 @@ val_loader = DataLoader(yoloCOCO(cfg, 'val'), batch_size=cfg.DATASET.BATCHSIZE, 
 opti = Adam(model.parameters(), lr=cfg.OPTIM['INIT_LR'])
 trainer = YolodetTrainer(cfg, model, opti, [train_loader, val_loader])
 trainer.set_device('cuda')
-trainer.run()
+
